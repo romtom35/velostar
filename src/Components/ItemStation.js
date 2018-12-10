c551e8c0cc1fdfa35673cf41f93a488f09e97d9c
romtom35 <romtom35@gmail.com>
2018-12-10 17:05:11 +0100

    Badge sur les stations

--- src/Components/ItemStation.js
+++ src/Components/ItemStation.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import { ListGroupItem } from 'react-bootstrap';
+import {Badge} from 'react-bootstrap';
 
 class ItemStation extends Component {
     render() {
         return (
-            <ListGroupItem>{this.props.station.nom}</ListGroupItem>
+            <ListGroupItem className='d-flex justify-content-between align-items-center'>
+                {this.props.station.nom}
+                <Badge bsStyle='primary'> {this.props.station.nombrevelosdisponibles}</Badge>
+            </ListGroupItem>
                );
     }
 }
